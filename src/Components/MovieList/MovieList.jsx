146c5273dbd997a6a7ad0d7fd6188d2558f76654
rasya9991 <racalii.rasya@gmail.com>
2023-03-03 23:38:42 +0700

    save

--- src/Components/MovieList/MovieList.jsx
+++ src/Components/MovieList/MovieList.jsx
@@ -5,7 +5,7 @@ const MovieList = (props) => {
     <div className={'movie-list'} style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', padding: '20px' }}>
       {props.children}
     </div>
-  )
+  );
 };
 
 export default MovieList;
